4e88294a2cc6f25b70a907e876a468057152efcd
himanusia <163955497+himanusia@users.noreply.github.com>
2026-06-13 20:52:03 +0700

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.ts
@@ -0,0 +1,13 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s: string): boolean {
+        const cleaned = s
+            .toLowerCase()
+            .replace(/[^a-z0-9]/g, "");
+
+        return cleaned === cleaned.split("").reverse().join("");
+    }
+}
